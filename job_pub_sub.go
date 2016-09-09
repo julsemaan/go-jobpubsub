55c1bb6958b87c93b8333fbb52c97728626c3b0f
Julien Semaan <jul.semaan@gmail.com>
2016-09-08 22:09:45 -0400

    move logic in its own struct

---
+++ job_pub_sub.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cskr/pubsub"
+	"sync"
+	"time"
+)
+
+type JobPubSub struct {
+	lock      *sync.Mutex
+	pubSubMap map[string]*pubsub.PubSub
+}
+
+func NewJobPubSub() *JobPubSub {
+	jps := &JobPubSub{}
+	jps.lock = &sync.Mutex{}
+	jps.pubSubMap = make(map[string]*pubsub.PubSub)
+	return jps
+}
+
+func (self *JobPubSub) Compute(id string, job func() interface{}) interface{} {
+
+	c := self.getSubChan(id)
+	// There is already someone taking care of it
+	if c != nil {
+		select {
+		case val := <-c:
+			return val
+		case <-time.After(time.Second * 2):
+			fmt.Println("Timeout reading from pubsub channel...")
+			return job()
+		}
+	}
+
+	// No one taking care of it so we compute it and publish it to any connected peer
+	result := job()
+	self.lock.Lock()
+	self.pubSubMap[id].Pub(result, "result")
+	self.pubSubMap[id] = nil
+	self.lock.Unlock()
+	return result
+}
+
+func (self *JobPubSub) getSubChan(id string) chan interface{} {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	var c chan interface{}
+	if self.pubSubMap[id] != nil {
+		c = self.pubSubMap[id].Sub("result")
+		fmt.Printf("Subscribing to existing computing for %s \n", id)
+	} else {
+		fmt.Printf("Creating new pubsub for %s \n", id)
+		ps := pubsub.New(1)
+		self.pubSubMap[id] = ps
+	}
+	return c
+}
--- main.go
+++ main.go
@@ -2,52 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/cskr/pubsub"
 	"sync"
 	"time"
 )
-
-var lock = &sync.Mutex{}
-var pubSubMap = make(map[string]*pubsub.PubSub)
-
-func compute(id string, job func() interface{}) interface{} {
-
-	c := getSubChan(id)
-	// There is already someone taking care of it
-	if c != nil {
-		select {
-		case val := <-c:
-			return val.(string)
-		case <-time.After(time.Second * 2):
-			fmt.Println("Timeout reading from pubsub channel...")
-			return job()
-		}
-	}
-
-	// No one taking care of it so we compute it and publish it to any connected peer
-	result := job()
-	lock.Lock()
-	pubSubMap[id].Pub(result, "result")
-	pubSubMap[id] = nil
-	lock.Unlock()
-	return result
-}
-
-func getSubChan(id string) chan interface{} {
-	lock.Lock()
-	defer lock.Unlock()
-
-	var c chan interface{}
-	if pubSubMap[id] != nil {
-		c = pubSubMap[id].Sub("result")
-		fmt.Printf("Subscribing to existing computing for %s \n", id)
-	} else {
-		fmt.Printf("Creating new pubsub for %s \n", id)
-		ps := pubsub.New(1)
-		pubSubMap[id] = ps
-	}
-	return c
-}
 
 func complexStuff(s string) string {
 	time.Sleep(1 * time.Second)
@@ -56,6 +13,7 @@ func complexStuff(s string) string {
 
 func main() {
 	MAX := 5
+	jps := NewJobPubSub()
 	var w sync.WaitGroup
 	w.Add(MAX)
 	//ps := pubsub.New(1)
@@ -66,7 +24,7 @@ func main() {
 			fmt.Printf("Starting routine %d \n", i)
 			//arg := fmt.Sprintf("bouzin%d", i)
 			arg := fmt.Sprintf("bouzin")
-			val := compute(fmt.Sprintf(arg), func() interface{} { return complexStuff(arg) })
+			val := jps.Compute(fmt.Sprintf(arg), func() interface{} { return complexStuff(arg) })
 			fmt.Printf("goroutine %d says '%s' \n", i, val)
 			w.Done()
 		}(i)
